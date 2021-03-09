dcf3fc244cc0a0c75cc2a139d46102915539082f
henrique dantas <henrique@hotmail.ph>
2021-03-08 22:14:37 -0300

    first model

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,10 @@ module.exports = {
 
     presets: [ 
         '@babel/preset-env',
-        '@babel/preset-react', 
-        ]
+        ['@babel/preset-react', { 
+            runtime: 'automatic'
+        }],
+    ]
 }
 
 
